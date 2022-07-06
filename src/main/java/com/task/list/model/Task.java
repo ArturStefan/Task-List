2a453596737c1301704387186095ef30c668b604
Artur Stefan <arthurstephan13@gmail.com>
2022-07-05 22:57:29 -0300

    Using lombok to generate getters and setters

--- src/main/java/com/task/list/model/Task.java
+++ src/main/java/com/task/list/model/Task.java
@@ -1,83 +1,56 @@
 package com.task.list.model;
 
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.Date;
+import java.util.UUID;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.task.list.dto.TaskDTO;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
-public class TaskList implements Serializable{
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Task implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(name = "id", updatable = false, nullable = false)
+	private UUID id;
+
 	private String name;
-	private String dateStart;
-	private String dateFinish;
+	@Temporal(TemporalType.DATE)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", locale = "pt-BR", timezone = "Brazil/East")
+	private Date dateStart;
+	@Temporal(TemporalType.DATE)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", locale = "pt-BR", timezone = "Brazil/East")
+	private Date dateFinish;
 	private String description;
 	private boolean finished;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getDateStart() {
-		return dateStart;
-	}
-	public void setDateStart(String dateStart) {
-		this.dateStart = dateStart;
-	}
-	public String getDateFinish() {
-		return dateFinish;
-	}
-	public void setDateFinish(String dateFinish) {
-		this.dateFinish = dateFinish;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public boolean isFinished() {
-		return finished;
-	}
-	public void setFinished(boolean finished) {
-		this.finished = finished;
-	}
-	@Override
-	public String toString() {
-		return "TaskList [id=" + id + ", name=" + name + ", dateStart=" + dateStart + ", dateFinish=" + dateFinish
-				+ ", Description=" + description + ", finished=" + finished + "]";
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(description, dateFinish, dateStart, finished, id, name);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		TaskList other = (TaskList) obj;
-		return Objects.equals(description, other.description) && Objects.equals(dateFinish, other.dateFinish)
-				&& Objects.equals(dateStart, other.dateStart) && finished == other.finished
-				&& Objects.equals(id, other.id) && Objects.equals(name, other.name);
+
+	public void transformDTOtoModel(TaskDTO taskDTO) {
+		this.name = taskDTO.getName();
+		this.dateFinish = taskDTO.getDateFinish();
+		this.description = taskDTO.getDescription();
+		this.dateStart = new Date();
+		this.finished = false;
 	}
 }
