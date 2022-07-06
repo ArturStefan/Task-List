876ec8cda3f88ee2567698413e189fcfbae21d8e
Artur Stefan <arthurstephan13@gmail.com>
2022-07-06 00:35:34 -0300

    removing unused imports

--- src/main/java/com/task/list/services/implement/TaskListServicesImplement.java
+++ src/main/java/com/task/list/services/implement/TaskListServicesImplement.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import com.task.list.dto.TaskDTO;
+import com.task.list.exception.ResourceNotFoundException;
 import com.task.list.model.Task;
 import com.task.list.repository.TaskListRepository;
 import com.task.list.services.TaskListServices;
@@ -26,19 +27,17 @@ public class TaskListServicesImplement implements TaskListServices {
 	public List<TaskDTO> findAll() {
 		List<Task> listTasks = repository.findAll();
 		List<TaskDTO> listTaskDTO = new ArrayList<>();
-		
-		listTasks.forEach(
-				task -> {
-					listTaskDTO.add(new TaskDTO().transformModelToDTO(task));
-				}
-		);
-		
+
+		listTasks.forEach(task -> {
+			listTaskDTO.add(new TaskDTO().transformModelToDTO(task));
+		});
+
 		return listTaskDTO;
 	}
 
 	@Override
 	public Task findById(UUID id) {
-		return repository.findById(id).orElse(new Task());
+		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Task não localizada"));
 	}
 
 	@Override
@@ -51,40 +50,33 @@ public class TaskListServicesImplement implements TaskListServices {
 
 	@Override
 	public ResponseEntity<Task> update(Task task) {
-		Task entity = repository.findById(task.getId()).orElse(new Task());
-		if (entity.getId() == null) {
-			return new ResponseEntity<Task>(HttpStatus.NOT_FOUND);
-		} 
-		else {
-			entity.setId(task.getId());
-			entity.setName(task.getName());
-			entity.setDateStart(task.getDateStart());
-			entity.setDateFinish(task.getDateFinish());
-			entity.setDescription(task.getDescription());
-			entity.setFinished(task.isFinished());
+		Task entity = repository.findById(task.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("Task não localizada"));
 
-			return new ResponseEntity<Task>(repository.save(entity),HttpStatus.OK);
-		}
-		
+		entity.setId(task.getId());
+		entity.setName(task.getName());
+		entity.setDateStart(task.getDateStart());
+		entity.setDateFinish(task.getDateFinish());
+		entity.setDescription(task.getDescription());
+		entity.setFinished(task.isFinished());
+
+		return new ResponseEntity<Task>(repository.save(entity), HttpStatus.OK);
 	}
 
 	@Override
 	public ResponseEntity<Task> delete(UUID id) {
-		repository.deleteById(id);
+		Task entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Task não localizada"));
+		
+		repository.delete(entity);
 		return new ResponseEntity<Task>(HttpStatus.NO_CONTENT);
 	}
 
 	@Override
 	public ResponseEntity<Task> updateFinished(UUID id) {
-		Task entity = repository.findById(id).orElse(new Task());
-		
-		if (entity.getId() == null) {
-			return new ResponseEntity<Task>(HttpStatus.NOT_FOUND);
-		} 
-		else {
-				entity.setFinished(true);
-				
-				return new ResponseEntity<Task>(repository.save(entity),HttpStatus.OK);
-			}
+		Task entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Task não localizada"));
+		entity.setFinished(true);
+
+		return new ResponseEntity<Task>(repository.save(entity), HttpStatus.OK);
 	}
 }
