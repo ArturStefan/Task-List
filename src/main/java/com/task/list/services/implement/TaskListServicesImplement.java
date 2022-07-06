5aa940b9027fc939257a5f7d908d8f8523be29e4
Artur Stefan <arthurstephan13@gmail.com>
2022-07-05 22:44:26 -0300

    Service implementation response change

--- src/main/java/com/task/list/services/implement/TaskListServicesImplement.java
+++ src/main/java/com/task/list/services/implement/TaskListServicesImplement.java
@@ -1,42 +1,75 @@
 package com.task.list.services.implement;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-import com.task.list.model.TaskList;
+import com.task.list.dto.TaskDTO;
+import com.task.list.model.Task;
 import com.task.list.repository.TaskListRepository;
 import com.task.list.services.TaskListServices;
 
 @Component
-public class TaskListServicesImplement implements TaskListServices{
-	
+public class TaskListServicesImplement implements TaskListServices {
+
+	private Task task;
+
 	@Autowired
 	TaskListRepository repository;
-	
+
 	@Override
-	public List<TaskList> findAll() {
-		return repository.findAll();
-	}
-	
-	@Override
-	public TaskList findById(Long id) {
-		return repository.findById(id).orElse(new TaskList());
+	public List<TaskDTO> findAll() {
+		List<Task> listTasks = repository.findAll();
+		List<TaskDTO> listTaskDTO = new ArrayList<>();
+		
+		listTasks.forEach(
+				task -> {
+					listTaskDTO.add(new TaskDTO().transformModelToDTO(task));
+				}
+		);
+		
+		return listTaskDTO;
 	}
 
 	@Override
-	public TaskList create(TaskList taskList) {
-		return repository.save(taskList);
+	public Task findById(UUID id) {
+		return repository.findById(id).orElse(new Task());
 	}
 
 	@Override
-	public TaskList update(TaskList taskList) {
-		return repository.save(taskList);
+	public ResponseEntity<Task> create(TaskDTO taskDTO) {
+		task = new Task();
+		task.transformDTOtoModel(taskDTO);
+		repository.save(task);
+		return new ResponseEntity<Task>(HttpStatus.CREATED);
 	}
 
 	@Override
-	public void delete(Long id) {
+	public ResponseEntity<Task> update(Task task) {
+		Task entity = repository.findById(task.getId()).orElse(new Task());
+		if (entity.getId() == null) {
+			return new ResponseEntity<Task>(HttpStatus.NOT_FOUND);
+		} else {
+			entity.setId(task.getId());
+			entity.setName(task.getName());
+			entity.setDateStart(task.getDateStart());
+			entity.setDateFinish(task.getDateFinish());
+			entity.setDescription(task.getDescription());
+			entity.setFinished(task.isFinished());
+
+			return new ResponseEntity<Task>(repository.save(entity),HttpStatus.OK);
+		}
+		
+	}
+
+	@Override
+	public ResponseEntity<Task> delete(UUID id) {
 		repository.deleteById(id);
+		return new ResponseEntity<Task>(HttpStatus.NO_CONTENT);
 	}
 }
