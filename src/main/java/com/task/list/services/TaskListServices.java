bc0e6a8625c3dfce40baa350166ac3f32999c348
Artur Stefan <arthurstephan13@gmail.com>
2022-07-05 22:43:13 -0300

    Service response change

--- src/main/java/com/task/list/services/TaskListServices.java
+++ src/main/java/com/task/list/services/TaskListServices.java
@@ -1,20 +1,23 @@
 package com.task.list.services;
 
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.task.list.model.TaskList;
+import com.task.list.dto.TaskDTO;
+import com.task.list.model.Task;
 
 @Service
 public interface TaskListServices {
-	public List<TaskList> findAll();
+	public List<TaskDTO> findAll();
 	
-	public TaskList findById(Long id);
+	public Task findById(UUID id);
 	
-	public TaskList create(TaskList taskList);
+	public ResponseEntity<Task> create(TaskDTO taskDTO);
 	
-	public TaskList update(TaskList taskList);
+	public ResponseEntity<Task> update(Task taskList);
 	
-	public void delete(Long id);
+	public ResponseEntity<Task> delete(UUID id);
 }
